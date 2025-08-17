90d6a541d6f322262fb8cb408a0cd30cdd902a51
ThatKid <thomas.cobiac@outlook.com>
2025-08-17 12:31:34 +0930

    player only lost heart if killed by another player.

--- src/main/kotlin/site/thatkid/soulBound/listeners/PlayerDeathListener.kt
+++ src/main/kotlin/site/thatkid/soulBound/listeners/PlayerDeathListener.kt
@@ -1,7 +1,5 @@
 package site.thatkid.soulBound.listeners
 
-import org.bukkit.Bukkit
-import org.bukkit.entity.Player
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 import org.bukkit.event.entity.PlayerDeathEvent
@@ -13,16 +11,12 @@ class PlayerDeathListener(private val plugin: JavaPlugin) : Listener {
     @EventHandler
     fun onPlayerDeath(event: PlayerDeathEvent) {
         val victim = event.entity
-        val killer = victim.killer as? Player ?: return
 
-        // Iterate a copy to avoid concurrent modification
         val heartsToDrop = ActiveHearts.getHearts(victim.uniqueId).toList()
 
         for (heart in heartsToDrop) {
-            // Add to vanilla drops so it shows with all other items
             event.drops.add(heart.createItem())
 
-            // Remove from active list immediately
             ActiveHearts.removeHeart(victim, heart)
         }
     }
