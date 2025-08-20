208fa7334881e7a8b9964ff30ca8efff72a12a02
ThatKid <thomas.cobiac@outlook.com>
2025-08-21 07:34:24 +0930

    DiscordBot.kt

--- src/main/kotlin/site/thatkid/soulBound/managers/DiscordBot.kt
+++ src/main/kotlin/site/thatkid/soulBound/managers/DiscordBot.kt
@@ -1,32 +1,45 @@
 package site.thatkid.soulBound.managers
 
+import net.axay.kspigot.runnables.task
+import org.bukkit.Bukkit
 import org.bukkit.plugin.java.JavaPlugin
+import org.java_websocket.client.WebSocketClient
+import org.java_websocket.handshake.ServerHandshake
 import java.net.HttpURLConnection
+import java.net.URI
 import java.net.URL
 
 
-class DiscordBot(private val plugin: JavaPlugin) {
+class DiscordBot(private val plugin: JavaPlugin, private val uri: URI) {
 
-    fun sendToDiscord(message: String) {
+    private var client: WebSocketClient? = null
+    private val pluginRef = plugin
+
+    init {
         try {
-            val url: URL = URL("http://BOT_IP:3000/mc") // change to BOT_IP + port - add a config in a .yml file maybe
-            val conn: HttpURLConnection = url.openConnection() as HttpURLConnection // Open a connection to the URL
-            conn.setRequestProperty("User-Agent", "Mozilla/5.0") // Set a user agent to avoid issues with some servers with a firewall that blocks non browser requests
-            conn.connectTimeout = 5000 // Set a timeout for the connection
-            conn.readTimeout = 5000 // Set a timeout for reading the response
-            conn.setRequestMethod("POST")
-            conn.setRequestProperty("Content-Type", "application/json")
-            conn.setDoOutput(true)
+            client = object : WebSocketClient(uri) {
+                override fun onOpen(handshakedata: ServerHandshake?) {
+                    plugin.logger.info("[DiscordBridge] Connected to Discord bot WebSocket!")
+                }
 
-            val json = "{\"content\":\"" + message + "\"}"
-            conn.getOutputStream().use { os ->
-                os.write(json.toByteArray())
+                override fun onMessage(message: String?) {
+                    message?.let {
+                        plugin.logger.info("[DiscordBridge] Received message: $it") // log to see if it works
+
+                        // add stuff we recieve later
+                    }
+                }
+
+                override fun onClose(code: Int, reason: String?, remote: Boolean) {
+                    Bukkit.getLogger().warning("[DiscordBridge] Connection closed: $reason")
+                }
+
+                override fun onError(ex: Exception?) {
+                    ex?.printStackTrace()
+                }
             }
-            conn.getResponseCode() // triggers the request
-            conn.disconnect()
-        } catch (error: Exception) {
-
-            error.printStackTrace()
+        } catch (e: Exception) {
+            e.printStackTrace()
         }
     }
 }
